623e9deb7510c07d52cbb00c587accb19c89a312
pavel.suvit <pavel.suvit@saltpay.co>
2023-06-12 13:33:45 +0100

    Move Command Line processing to background thread

--- src/main/kotlin/com/example/demo1/DemoViewModel.kt
+++ src/main/kotlin/com/example/demo1/DemoViewModel.kt
@@ -3,8 +3,11 @@ package com.example.demo1
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.ScriptRunnerUtil
 import com.intellij.openapi.project.Project
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import java.io.File
 import java.nio.charset.Charset
 
@@ -13,14 +16,16 @@ class DemoViewModel(private val project: Project?) {
     val output = _output.asStateFlow()
 
     fun processCommandLine(input: String) {
-        _output.tryEmit(try {
-            val commands = input.split(" ")
-            val generalCommandLine = GeneralCommandLine(commands)
-            generalCommandLine.charset = Charset.forName("UTF-8")
-            generalCommandLine.workDirectory = File(project?.basePath ?: project.toString())
-            ScriptRunnerUtil.getProcessOutput(generalCommandLine)
-        } catch (t: Throwable) {
-            "Invalid command"
-        })
+        CoroutineScope(Dispatchers.IO).launch {
+            _output.emit(try {
+                val commands = input.split(" ")
+                val generalCommandLine = GeneralCommandLine(commands)
+                generalCommandLine.charset = Charset.forName("UTF-8")
+                generalCommandLine.workDirectory = File(project?.basePath ?: project.toString())
+                ScriptRunnerUtil.getProcessOutput(generalCommandLine)
+            } catch (t: Throwable) {
+                "Invalid command"
+            })
+        }
     }
 }
